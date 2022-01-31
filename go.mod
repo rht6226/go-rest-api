d21ef4d39931552a17a153c17ff1f5d40bada7f7
Rohit Anand <rohitraazanand567@gmail.com>
2022-01-31 22:51:51 +0530

    tested service

--- go.mod
+++ go.mod
@@ -7,11 +7,15 @@ require github.com/gorilla/mux v1.8.0
 require (
 	cloud.google.com/go v0.97.0 // indirect
 	cloud.google.com/go/firestore v1.6.1 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
 	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
@@ -23,4 +27,5 @@ require (
 	google.golang.org/genproto v0.0.0-20211028162531-8db9c33dc351 // indirect
 	google.golang.org/grpc v1.40.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ service/post-service_test.go
@@ -0,0 +1,91 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/rht6226/go-rest-api/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// Mock Repository
+type mockRepository struct {
+	mock.Mock
+}
+
+func (mock *mockRepository) Save(post *entity.Post) (*entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
+func (mock *mockRepository) FindAll() ([]entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Post), args.Error(1)
+}
+
+// Case when empty post is supplied
+func TestValidateEmptyPost(t *testing.T) {
+	testService := NewPostService(nil)
+
+	err := testService.Validate(nil)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "the post is empty", err.Error())
+}
+
+// case when empty title is supplied
+func TestValidateEmptyPostTitle(t *testing.T) {
+	post := entity.Post{Id: 1, Title: "", Text: "B"}
+	testService := NewPostService(nil)
+
+	err := testService.Validate(&post)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "the post title is empty", err.Error())
+}
+
+// Find all
+func TestFindAll(t *testing.T) {
+	mockRepo := new(mockRepository)
+
+	var id int64 = 1
+	post := entity.Post{Id: id, Title: "A", Text: "B"}
+
+	// setup Expectations
+	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
+
+	testService := NewPostService(mockRepo)
+
+	result, _ := testService.FindAll()
+
+	// Mock Assertion: Behavioural
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.Equal(t, id, result[0].Id)
+	assert.Equal(t, "A", result[0].Title)
+	assert.Equal(t, "B", result[0].Text)
+}
+
+// Create
+func TestCreate(t *testing.T) {
+	mockRepo := new(mockRepository)
+	post := entity.Post{Title: "A", Text: "B"}
+
+	// setup Expectation
+	mockRepo.On("Save").Return(&post, nil)
+
+	testService := NewPostService(mockRepo)
+
+	result, err := testService.Create(&post)
+
+	// Mock Assertion: Behavioural
+	mockRepo.AssertExpectations(t)
+
+	// Data Assertion
+	assert.NotNil(t, result.Id)
+	assert.Equal(t, "A", result.Title)
+	assert.Equal(t, "B", result.Text)
+	assert.Nil(t, err)
+}
